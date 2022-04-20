eddee520fa81d853a6e83f875f349d350faf0783
chae <stat_chae0@sookmyung.ac.kr>
2022-04-20 16:08:10 +0900

    Http 요청 파라미터 - @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,16 +25,16 @@ public class RequestParamController {
 
         response.getWriter().write("ok");
     }
-    
+
     @ResponseBody //레스트 컨트롤러랑 같은 역할 하게 함 리턴을 바로 html로 띄움
     @RequestMapping("/request-param-v2")
     public String requestParamV2(
             @RequestParam("username") String memberName,
-            @RequestParam("age")    int memberAge
-    ){
+            @RequestParam("age") int memberAge
+    ) {
         log.info("username={}, age={}", memberName, memberAge);
         return "ok";
-        
+
     }
 
     @ResponseBody
@@ -40,7 +42,7 @@ public class RequestParamController {
     public String requestParamV3(
             @RequestParam String username,
             @RequestParam int age //이름같으면 생략가능
-    ){
+    ) {
         log.info("username={}, age={}", username, age);
         return "ok";
 
@@ -48,7 +50,7 @@ public class RequestParamController {
 
     @ResponseBody
     @RequestMapping("/request-param-v4")
-    public String requestParamV4(String username,int age ){//다 생략가능^_^ 근데 넣는게 좋다고 생각하심
+    public String requestParamV4(String username, int age) {//다 생략가능^_^ 근데 넣는게 좋다고 생각하심
         log.info("username={}, age={}", username, age);
         return "ok";
     }
@@ -61,7 +63,7 @@ public class RequestParamController {
 //        int a = null -> 오류
 //        Integer a= null -> 오류 안남
             //null,""은 다른것
-    ){
+    ) {
         log.info("username={}, age={}", username, age);
         return "ok";
     }
@@ -72,15 +74,29 @@ public class RequestParamController {
     public String requestParamDefault(
             @RequestParam(required = true, defaultValue = "guest") String username,
             @RequestParam(required = false, defaultValue = "-1") Integer age //디폴트벨류가 있으면 리콰이어드 의미없어 빈문자도 처리해줌
-    ){
+    ) {
         log.info("username={}, age={}", username, age);
         return "ok";
     }
 
     @ResponseBody
     @RequestMapping("/request-param-map")
-    public String requestParamMap(@RequestParam Map<String, Object> paramMap){
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+//    public String modelAttributeV1(@ModelAttribute HelloData helloData){ ModelAttribute 생략 가능
+    public String modelAttributeV1(HelloData helloData){ // @ModelAttribute 생략 가능
+//        HelloData helloData = new HelloData();
+//        helloData.setUsername(username);
+//        helloData.setAge(age);
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData = {}", helloData);
+
         return "ok";
     }
 }
