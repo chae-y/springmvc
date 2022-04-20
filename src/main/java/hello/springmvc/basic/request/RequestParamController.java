fe04deab08a40fa614de2c13f7ec8232908e6162
chae <stat_chae0@sookmyung.ac.kr>
2022-04-20 14:19:37 +0900

    Http 요청 - @RequestParam

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -3,10 +3,13 @@ package hello.springmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,5 +22,65 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+    
+    @ResponseBody //레스트 컨트롤러랑 같은 역할 하게 함 리턴을 바로 html로 띄움
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age")    int memberAge
+    ){
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+        
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age //이름같으면 생략가능
+    ){
+        log.info("username={}, age={}", username, age);
+        return "ok";
+
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username,int age ){//다 생략가능^_^ 근데 넣는게 좋다고 생각하심
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username, //디폴트 트루
+            @RequestParam(required = false) Integer age //폴스면 없어도 된다는 뜻 트루는 무조건 있어야함 없으면 오류 인트는 널이 들어갈 수 없음
+//        int a = null -> 오류
+//        Integer a= null -> 오류 안남
+            //null,""은 다른것
+    ){
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") Integer age //디폴트벨류가 있으면 리콰이어드 의미없어 빈문자도 처리해줌
+    ){
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap){
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
     }
 }
